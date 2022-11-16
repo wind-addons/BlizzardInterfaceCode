7b8f45375d52da86d917fd280520580c3996a858
fang2hou <fang2hou@outlook.com>
2022-11-16 11:29:35 +0900

    10.0.2.4668

--- Interface/AddOns/Blizzard_InspectUI/Localization.lua
+++ Interface/AddOns/Blizzard_InspectUI/Localization.lua
@@ -2,5 +2,5 @@
 
 for i=1, (InspectFrame.numTabs or 0) do
 	local tabName = "InspectFrameTab"..i;
-	_G[tabName.."Text"]:SetPoint("CENTER", tabName, "CENTER", 0, 5);
+	_G[tabName].Text:SetPoint("CENTER", tabName, "CENTER", 0, 5);
 end
